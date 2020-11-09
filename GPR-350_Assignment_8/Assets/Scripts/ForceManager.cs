e9914db2ed3731e1b217c26646f0a4ba27441dbe
Nicholas (Hoy) Perell <nicholas.hoy@gmail.com>
2020-11-08 22:16:53 -0500

    A whole lotta updates

--- GPR-350_Assignment_8/Assets/Scripts/ForceManager.cs
+++ GPR-350_Assignment_8/Assets/Scripts/ForceManager.cs
@@ -6,30 +6,31 @@ public class ForceManager : MonoBehaviour
 {
     static List<ForceGenerator2D> forceGenerators;
 
-    static void AddForceGenerator(ref ForceGenerator2D fg)
+    static public void AddForceGenerator(ref ForceGenerator2D fg)
     {
         forceGenerators.Add(fg);
     }
 
-    static void DeleteForceGenerator(ref ForceGenerator2D fg)
+    static public void DeleteForceGenerator(ref ForceGenerator2D fg)
     {
         forceGenerators.Remove(fg);
     }
 
-    static void ApplyAllForces(float dt)
+    static public void ApplyAllForces(float dt)
     {
         foreach(ForceGenerator2D fg in forceGenerators)
         {
             if(fg.GetShouldEffectAll())
             {
-                foreach(Particle2D particle2D in FindObjectsOfType<Particle2D>())
+                foreach(Particle2D particle2D in GameObject.FindObjectsOfType<Particle2D>())
                 {
-                    //fg.UpdateForce(particle2D, dt);
+                    fg.UpdateForce(ref particle2D.mpPhysicsData, dt);
                 }
             }
             else
             {
-                //fg.UpdateForce(new PhysicsData2D(), dt);
+                PhysicsDataPtr p = new PhysicsDataPtr();
+                fg.UpdateForce(ref p, dt);
             }
         }
     }
--- GPR-350_Assignment_8/Assets/Scripts/PointForceGenerator2D.cs
+++ GPR-350_Assignment_8/Assets/Scripts/PointForceGenerator2D.cs
@@ -24,7 +24,7 @@ public class PointForceGenerator2D : ForceGenerator2D
     public override void UpdateForce(ref PhysicsDataPtr pData, float dt)
     {
         Vector2 diff = point - pData.pos;
-        if (diff.magnitude < range)
+        if (diff.sqrMagnitude < range * range)
         {
             float dist = diff.magnitude;
             float proportionAway = dist / range;
