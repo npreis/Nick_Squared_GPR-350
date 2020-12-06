5d01c9453b0544d45e6a7b562fbb7215e1cf5388
Nick <nicholas.preis@mymail.champlain.edu>
2020-12-06 18:44:20 -0500

    Made a script that spawns in an object when you click.

---
+++ GPR-350_Final/GPR-350_Final/Assets/Scripts/ObjectSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class ObjectSpawn : MonoBehaviour
+{
+    public GameObject asteroid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Vector3 origin = Camera.main.ScreenToWorldPoint(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
+            Vector3 changeZ = new Vector3(origin.x, origin.y, asteroid.transform.position.z);
+
+            Instantiate(asteroid).transform.position = changeZ;
+        }
+    }
+}
