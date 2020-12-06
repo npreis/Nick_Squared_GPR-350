41b33dc1a24ea65801d24b3f8adc883c9e4acf2f
Nick <nicholas.preis@mymail.champlain.edu>
2020-12-06 17:54:52 -0500

    Camera now scrolls in and out.

--- GPR-350_Final/GPR-350_Final/Assets/Scripts/CameraMovement.cs
+++ GPR-350_Final/GPR-350_Final/Assets/Scripts/CameraMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Threading;
 using UnityEngine;
@@ -8,6 +9,7 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public float mSpeed = 3.0f;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +25,10 @@ public class CameraMovement : MonoBehaviour
 
     void Move()
     {
-        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        var d = Input.GetAxis("Mouse ScrollWheel");
+        var zoom = 0.1f;
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             transform.Translate(new Vector3(mSpeed * Time.deltaTime, 0, 0));
         }
@@ -38,6 +43,14 @@ public class CameraMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
             transform.Translate(new Vector3(0, -mSpeed * Time.deltaTime, 0));
+        }
+        if(d > 0)
+        {
+            Camera.main.orthographicSize -= zoom;
+        }
+        if(d < 0)
+        {
+            Camera.main.orthographicSize += zoom;
         }
     }
 }
