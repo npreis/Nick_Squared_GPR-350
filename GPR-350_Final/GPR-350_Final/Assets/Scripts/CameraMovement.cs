ba135e1b5674305b76f27a604b0110b9642a6643
Nick <nicholas.preis@mymail.champlain.edu>
2020-12-06 17:40:57 -0500

    Camera now moves with arrow keys and WASD.

--- GPR-350_Final/GPR-350_Final/Assets/Scripts/CameraMovement.cs
+++ GPR-350_Final/GPR-350_Final/Assets/Scripts/CameraMovement.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
@@ -20,5 +23,21 @@ public class CameraMovement : MonoBehaviour
 
     void Move()
     {
+        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(mSpeed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-mSpeed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, mSpeed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, -mSpeed * Time.deltaTime, 0));
+        }
     }
 }
