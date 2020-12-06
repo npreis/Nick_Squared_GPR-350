5c09bb0d17d3350e733decdc1d86faa2a176b18f
Nick <nicholas.preis@mymail.champlain.edu>
2020-12-06 17:25:54 -0500

    Created a camera movement script.

---
+++ GPR-350_Final/GPR-350_Final/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float mSpeed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+    }
+}
